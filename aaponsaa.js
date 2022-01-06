e5036929dbaf12cc7d5645462c0be04aae2963d8
Aapo Kiviniemi <93884246+AapoStone@users.noreply.github.com>
2022-01-06 16:10:19 +0200

    Karttakuvien lisäys + turhien kommenttien poisto

--- aaponsaa.js
+++ aaponsaa.js
@@ -7,131 +7,137 @@ document.querySelector("#posio").addEventListener("click", posio);
 
 
 function rovaniemi(){
-//Lets create a HTTP request
+
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Rovaniemi&units=metric&mode=XML&APPID=ff64c247a136f706923d1ee0d55d71e2",true);
-//Send request
+
 xmlhttp.send();
 
 
-//Lets creata an event handler
+
 xmlhttp.onreadystatechange=function() {
     if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
 
-        //Response is ready
-        //Lets parse JSON response
+        
         let weatherObject = JSON.parse(xmlhttp.response);
 
-    //Lets add text    
+      
     document.getElementById("main").innerHTML = weatherObject.weather[0].main;
     document.getElementById("description").innerHTML = weatherObject.weather[0].description;
     document.getElementById("temperature").innerHTML = weatherObject.main.temp + " &#8451;";
     document.getElementById("feelslike").innerHTML = weatherObject.main.feels_like + " &#8451;";
     document.getElementById("windspeed").innerHTML = weatherObject.wind.speed + " m/s";
+
+    document.getElementById("suomi").src=("images/saakarttaRovaniemi.png");
+
     }
     }
 }
 
 function oulu(){
-    //Lets create a HTTP request
+    
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Oulu&units=metric&mode=XML&APPID=ff64c247a136f706923d1ee0d55d71e2",true);
-    //Send request
+    
     xmlhttp.send();
     
     
-    //Lets creata an event handler
+   
     xmlhttp.onreadystatechange=function() {
         if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
     
-            //Response is ready
-            //Lets parse JSON response
+            
             let weatherObject = JSON.parse(xmlhttp.response);
     
-        //Lets add text    
+          
         document.getElementById("main").innerHTML = weatherObject.weather[0].main;
         document.getElementById("description").innerHTML = weatherObject.weather[0].description;
         document.getElementById("temperature").innerHTML = weatherObject.main.temp + " &#8451;";
         document.getElementById("feelslike").innerHTML = weatherObject.main.feels_like + " &#8451;";
         document.getElementById("windspeed").innerHTML = weatherObject.wind.speed + " m/s";
+
+        document.getElementById("suomi").src=("images/saakarttaOulu.png");
         }
         }
     }
 
     function kemijärvi(){
-        //Lets create a HTTP request
+        
         let xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Kemijärvi&units=metric&mode=XML&APPID=ff64c247a136f706923d1ee0d55d71e2",true);
-        //Send request
+        
         xmlhttp.send();
         
         
-        //Lets creata an event handler
+        
         xmlhttp.onreadystatechange=function() {
             if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
         
-                //Response is ready
-                //Lets parse JSON response
+                
                 let weatherObject = JSON.parse(xmlhttp.response);
         
-            //Lets add text    
+                
             document.getElementById("main").innerHTML = weatherObject.weather[0].main;
             document.getElementById("description").innerHTML = weatherObject.weather[0].description;
             document.getElementById("temperature").innerHTML = weatherObject.main.temp + " &#8451;";
             document.getElementById("feelslike").innerHTML = weatherObject.main.feels_like + " &#8451;";
             document.getElementById("windspeed").innerHTML = weatherObject.wind.speed + " m/s";
+            
+            document.getElementById("suomi").src=("images/saakarttaKemijarvi.png");
             }
             }
         }
 
         function helsinki(){
-            //Lets create a HTTP request
+            
             let xmlhttp = new XMLHttpRequest();
             xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&units=metric&mode=XML&APPID=ff64c247a136f706923d1ee0d55d71e2",true);
-            //Send request
+            
             xmlhttp.send();
             
             
-            //Lets creata an event handler
+            
             xmlhttp.onreadystatechange=function() {
                 if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
             
-                    //Response is ready
-                    //Lets parse JSON response
+                    
                     let weatherObject = JSON.parse(xmlhttp.response);
             
-                //Lets add text    
+                    
                 document.getElementById("main").innerHTML = weatherObject.weather[0].main;
                 document.getElementById("description").innerHTML = weatherObject.weather[0].description;
                 document.getElementById("temperature").innerHTML = weatherObject.main.temp + " &#8451;";
                 document.getElementById("feelslike").innerHTML = weatherObject.main.feels_like + " &#8451;";
                 document.getElementById("windspeed").innerHTML = weatherObject.wind.speed + " m/s";
+
+                document.getElementById("suomi").src=("images/saakarttaHelsinki.png");
                 }
                 }
             }
 
             function posio(){
-                //Lets create a HTTP request
+                
                 let xmlhttp = new XMLHttpRequest();
                 xmlhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Posio&units=metric&mode=XML&APPID=ff64c247a136f706923d1ee0d55d71e2",true);
-                //Send request
+                
                 xmlhttp.send();
                 
                 
-                //Lets creata an event handler
+                
                 xmlhttp.onreadystatechange=function() {
                     if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
                 
-                        //Response is ready
-                        //Lets parse JSON response
+                        
                         let weatherObject = JSON.parse(xmlhttp.response);
                 
-                    //Lets add text    
+                        
                     document.getElementById("main").innerHTML = weatherObject.weather[0].main;
                     document.getElementById("description").innerHTML = weatherObject.weather[0].description;
                     document.getElementById("temperature").innerHTML = weatherObject.main.temp + " &#8451;";
                     document.getElementById("feelslike").innerHTML = weatherObject.main.feels_like + " &#8451;";
                     document.getElementById("windspeed").innerHTML = weatherObject.wind.speed + " m/s";
+
+                    document.getElementById("suomi").src=("images/saakarttaPosio.png");
                     }
                     }
                 }
